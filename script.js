4ac346e56260e46dc2092aaf3604b492dd334620
Katasravani <105917453+Katasravani@users.noreply.github.com>
2023-11-24 11:22:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ let a=prompt("Enter first number");
 let res=parseInt(a)*2;
 let b=prompt("Enter the second number");
 res=res+parseInt(b);
-alert("The final result is:  " + res);
+alert("The final result is: " + res);
 
